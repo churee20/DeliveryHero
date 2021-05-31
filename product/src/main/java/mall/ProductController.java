8cc2c7ac2c847275c5eff8b2362dc782ed90265d
churee20 <churee20@gmail.com>
2021-05-31 12:51:29 +0900

    first commit

---
+++ product/src/main/java/mall/ProductController.java
@@ -0,0 +1,43 @@
+package mall;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+ @RestController
+ public class ProductController {
+
+    @Autowired 
+    ProductRepository productRepository;
+
+    @RequestMapping(value = "/products/checkAndModifyStock",
+            method = RequestMethod.GET,
+            produces = "application/json;charset=UTF-8")
+    public boolean checkAndModifyStock(@RequestParam("productId") Long productId, 
+                                        @RequestParam("qty")  int qty) 
+
+            System.out.println("##### /products/checkAndModifyStock  called #####");                                        
+
+            throws Exception {
+
+            System.out.println("##### /products checked product Status #####");
+
+            boolean status = false;
+            Optional<Product> productOptional = productRepository.findByProductId(productId);
+            Product product = productOptional.get();
+
+            if (product.getStock() >= qty) {
+                    status = true;
+                    product.setStock(product.getStock() - qty);
+                    productRepository.save(product);
+                    
+            System.out.println("##### Order Accepted #####");
+
+            }
+            return status;
+    }   
+
+ }
